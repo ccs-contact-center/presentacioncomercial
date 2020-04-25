e7f618014402a78c5b36619a552aaa9d8dfb1730
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-04-24 23:41:04 -0500

    Update SIM_Facturacion.js

--- src/views/Areas/SIM_Facturacion.js
+++ src/views/Areas/SIM_Facturacion.js
@@ -11,28 +11,28 @@ class Comercial extends Component {
   render() {
     return (
       <div className="section" id="implementacion">
-        <div className="content">
+        <div className="content" style={{padding:'20px'}}>
           <h1>Datos Financieros</h1>
-          <br />
+        
           <p style={{ margin: "-1vw" }}>
             Durante los últimos 9 años, Grupo SIM ha logrado crecimientos de
             doble digito tanto en ingresos como en nómina mensual procesada
           </p>
           <div className="info-box">
             <Zoom down delay={750}>
-              <div className="info-item" style={{ margin: "-1vw" }}>
+              <div className="info-item" >
                 <h2>Nómina Procesada Mensualmente</h2>
               </div>
             </Zoom>
             <Zoom left>
-              <div className="info-item" style={{ margin: "-1vw" }}>
+              <div className="info-item" >
                 <h2>Ingreso Anual</h2>
               </div>
             </Zoom>
             <Zoom rigth delay={500}>
               <div
                 className="info-item"
-                style={{ margin: "-1vw", marginTop: "2vw" }}
+                style={{ margin: "-1vw",  }}
               >
                 <img
                   src={people}
@@ -45,7 +45,7 @@ class Comercial extends Component {
             <Zoom up delay={250}>
               <div
                 className="info-item"
-                style={{ margin: "-1vw", marginTop: "2vw" }}
+                style={{ margin: "-1vw",  }}
               >
                 <img src={money} width={300} alt="logo" className="img-fluid" />
               </div>
@@ -53,15 +53,14 @@ class Comercial extends Component {
           </div>
 
           <Zoom>
-            <br />
-            <br />
+           
             <p>Estados financieros auditados por:</p>
             <img
               src={deloitte}
               width={200}
               alt="logo"
               className="img-fluid"
-              style={{ margin: "-2vw" }}
+              style={{ margin: "-1vw" }}
             />
           </Zoom>
         </div>
