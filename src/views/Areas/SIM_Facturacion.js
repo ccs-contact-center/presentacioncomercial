c8d208fd1748ca8da7f892fbfcb8ec431088f0e6
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-04-27 18:07:55 -0500

    Update SIM_Facturacion.js

--- src/views/Areas/SIM_Facturacion.js
+++ src/views/Areas/SIM_Facturacion.js
@@ -1,72 +1,74 @@
-import React, { Component } from "react";
-import "../../index.css";
-import "react-circular-progressbar/dist/styles.css";
+import React, { Component } from 'react'
+import '../../index.css'
+import 'react-circular-progressbar/dist/styles.css'
 
-import Zoom from "react-reveal/Zoom";
-import money from "../../assets/img/icons/money.png";
-import people from "../../assets/img/icons/people.png";
-import deloitte from "../../assets/img/partners/deloitte.png";
+import Zoom from 'react-reveal/Zoom'
+import money from '../../assets/img/icons/money.png'
+import people from '../../assets/img/icons/people.png'
+import deloitte from '../../assets/img/partners/deloitte.png'
 
 class Comercial extends Component {
   render() {
     return (
       <div className="section" id="implementacion">
-        <div className="content" style={{padding:'20px'}}>
-          <h1>Datos Financieros</h1>
-        
-          <p style={{ margin: "-1vw" }}>
-            Durante los últimos 9 años, Grupo SIM ha logrado crecimientos de
-            doble digito tanto en ingresos como en nómina mensual procesada
-          </p>
-          <div className="info-box">
-            <Zoom down delay={750}>
-              <div className="info-item" >
-                <h2>Nómina Procesada Mensualmente</h2>
+        <div className="content" style={{ padding: '20px' }}>
+          <div className="centrado-fila">
+            <div>
+              <h1>Datos Financieros</h1>
+
+              <p style={{ margin: '-1vw' }}>
+                Durante los últimos 9 años, Grupo SIM ha logrado crecimientos de
+                doble digito tanto en ingresos como en nómina mensual procesada
+              </p>
+              <div className="info-box" style={{marginTop:'10px'}}>
+                <Zoom down delay={750}>
+                  <div className="info-item" style={{marginBottom:'0px' }}>
+                    <h2>Nómina Procesada Mensualmente</h2>
+                  </div>
+                </Zoom>
+                <Zoom left>
+                  <div className="info-item" style={{marginBottom:'0px' }}>
+                    <h2>Ingreso Anual</h2>
+                  </div>
+                </Zoom>
+                <Zoom rigth delay={500}>
+                  <div className="info-item" style={{marginBottom:'3px' }}>
+                    <img
+                      src={people}
+                      width={300}
+                      alt="logo"
+                      className="img-fluid"
+                    />
+                  </div>
+                </Zoom>
+                <Zoom up delay={250}>
+                  <div className="info-item" style={{marginBottom:'3px' }}>
+                    <img
+                      src={money}
+                      width={300}
+                      alt="logo"
+                      className="img-fluid"
+                    />
+                  </div>
+                </Zoom>
               </div>
-            </Zoom>
-            <Zoom left>
-              <div className="info-item" >
-                <h2>Ingreso Anual</h2>
-              </div>
-            </Zoom>
-            <Zoom rigth delay={500}>
-              <div
-                className="info-item"
-                style={{ margin: "-1vw",  }}
-              >
+
+              <Zoom>
+                <p style={{marginBottom:'0px' }}>Estados financieros auditados por:</p>
                 <img
-                  src={people}
-                  width={300}
+                  src={deloitte}
+                  width={220}
                   alt="logo"
                   className="img-fluid"
+                 
                 />
-              </div>
-            </Zoom>
-            <Zoom up delay={250}>
-              <div
-                className="info-item"
-                style={{ margin: "-1vw",  }}
-              >
-                <img src={money} width={300} alt="logo" className="img-fluid" />
-              </div>
-            </Zoom>
+              </Zoom>
+            </div>
           </div>
-
-          <Zoom>
-           
-            <p>Estados financieros auditados por:</p>
-            <img
-              src={deloitte}
-              width={200}
-              alt="logo"
-              className="img-fluid"
-              style={{ margin: "-1vw" }}
-            />
-          </Zoom>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Comercial;
+export default Comercial
